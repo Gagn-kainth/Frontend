06535cebe50adc2689aadbd15c4e0eac4a2e510d
Gagn-kainth <preetgagan710@gmail.com>
2026-09-08 08:40:08 +0530

    added  groundcard

--- src/pages/BookingGround.jsx
+++ src/pages/BookingGround.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import BookingGroundHero from "../components/BookingGroundHero";
 import DateSelector from "../components/DateSelector";
+import GroundCard, { GROUNDS } from "../components/GroundCard";
+
 function BookingGround() {
+  const [selectedId, setSelectedId] = useState(null);
+
   return (
     <>
       <BookingGroundHero />
-      <DateSelector/>
+      <DateSelector />
+      <div className="bc-ground-list"  style={{
+            backgroundImage: `linear-gradient(to top, #000000`}}>
+        {GROUNDS.map((ground) => (
+          <GroundCard
+            key={ground.id}
+            ground={ground}
+            isSelected={selectedId === ground.id}
+            onSelect={setSelectedId}
+          />
+        ))}
+      </div>
     </>
   );
 }
