ade715c3483f89c8c798b729cfb8f18891218b3a
Gagn-kainth <preetgagan710@gmail.com>
2026-09-12 23:41:20 +0530

    basic setup is done

---
+++ Backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
