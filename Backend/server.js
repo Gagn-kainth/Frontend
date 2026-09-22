48a5b42e5ea4fc8811f5ba9afb2e311b10f0288b
Gagn-kainth <preetgagan710@gmail.com>
2026-09-22 15:00:24 +0530

    deployment of db on atlas

--- Backend/server.js
+++ Backend/server.js
@@ -3,37 +3,41 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
-
-
 dotenv.config();
-connectDB();
+
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
+app.use(express.json());
 
 const groundRoutes = require("./routes/groundRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
 
-app.use(
-    cors({
-      origin: "http://localhost:5173",
-    })
-  );
-
-  app.use(express.json());
-
 app.use("/api/grounds", groundRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/payments", paymentRoutes);
 
-
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(err.status || 500).json({
-      message: err.message || "Something went wrong",
-    });
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    message: err.message || "Something went wrong",
   });
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+async function startServer() {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+startServer();
