6e0b92ff0c1e1062549ba1b1b03bfae093e1fa90
Gagn-kainth <preetgagan710@gmail.com>
2026-09-24 14:15:45 +0530

    try fix the bugs

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://frontend-git-main-gagn-s-projects.vercel.app",
+  "https://frontend-git-main-gagn-s-projects.vercel.app",  "https://boundaryclub.vercel.app",
 ];
 
 app.use(
